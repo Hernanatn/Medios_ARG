8f2daf0be197f22653eb332d84f4da248a369960
Herni <hernanatn@gmail.com>
2023-09-26 20:33:15 -0300

    corregido init de dependencias

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
-from dependencias.utiles.utilesBS4 import *
-from dependencias.Notas import *
-from dependencias.utiles import *
-from dependencias.utiles import *
-
-
 from bs4 import BeautifulSoup, ResultSet, Tag, NavigableString
 from requests import get as GET
+
+
+from dependencias.utiles.utilesBS4 import *
+from dependencias.Notas import *
+from dependencias.Informes import *
+from dependencias.Medios import *
+
+
